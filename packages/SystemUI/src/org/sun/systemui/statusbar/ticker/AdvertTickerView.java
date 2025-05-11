SunOS-Project/android_sun-sdk

SystemUI: Port status bar ticker/lyric from Flyme
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

SystemUI/src/org/nameless/systemui/statusbar/ticker/AdvertTickerView.java
@@ -1,0 +1,157 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.systemui.statusbar.ticker;
+
+import static android.graphics.PorterDuff.Mode.SRC_IN;
+
+import static org.nameless.os.DebugConstants.DEBUG_TICKER;
+
+import android.app.Notification;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.service.notification.StatusBarNotification;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.plugins.DarkIconDispatcher;
+import com.android.systemui.res.R;
+
+import java.util.ArrayList;
+
+public class AdvertTickerView extends LinearLayout implements DarkIconDispatcher.DarkReceiver {
+
+    private static String TAG = "Ticker::AdvertTickerView";
+
+    private static String KEY_TICKER_ICON = "ticker_icon";
+    private static String KEY_TICKER_ICON_SWITCH = "ticker_icon_switch";
+
+    private ImageSwitcher mIconSwitcher;
+    private TextSwitcher mTextSwitcher;
+
+    private Drawable mTickerIconDrawable;
+    private int mTickIconId;
+
+    private String mCurrentKey;
+    private String mCurrentPkg;
+
+    public AdvertTickerView(Context context) {
+        super(context);
+    }
+
+    public AdvertTickerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AdvertTickerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).addDarkReceiver(this);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).removeDarkReceiver(this);
+    }
+
+    public boolean addNotification(StatusBarNotification sbn) {
+        if (sbn == null) {
+            return false;
+        }
+        final Notification notification = sbn.getNotification();
+        final boolean show = (notification.flags & Notification.FLAG_ALWAYS_SHOW_TICKER) != 0
+                && notification.tickerText != null && !sbn.isClearable();
+        if (DEBUG_TICKER) {
+            Log.d(TAG, "id=" + sbn.getId() + ", show=" + show
+                    + ", tickerText=" + notification.tickerText
+                    + ", key=" + sbn.getKey() + ", currentKey=" + mCurrentKey);
+        }
+        if (mCurrentKey != null && !sbn.getKey().equals(mCurrentKey)) {
+            return false;
+        }
+        if (!show) {
+            removeNotification(sbn.getKey());
+            return false;
+        }
+
+        mCurrentKey = sbn.getKey();
+        mCurrentPkg = sbn.getPackageName();
+        mTickerIconDrawable = notification.getSmallIcon().loadDrawable(mContext);
+        mIconSwitcher.setVisibility(View.VISIBLE);
+        mIconSwitcher.setImageDrawable(mTickerIconDrawable);
+        mTextSwitcher.setText(notification.tickerText);
+
+        final MarqueeTextView currentTicker =
+                (MarqueeTextView) mTextSwitcher.getChildAt(mTextSwitcher.getDisplayedChild());
+        if (currentTicker != null) {
+            currentTicker.startScrollSoon();
+        }
+        return true;
+    }
+
+    public void removeNotification(String key) {
+        if (mCurrentKey != null && mCurrentKey.equals(key)) {
+            mCurrentKey = null;
+            mTickerIconDrawable = null;
+            mIconSwitcher.setImageDrawable(null);
+            mIconSwitcher.setVisibility(View.GONE);
+            mTextSwitcher.setText(null);
+        }
+    }
+
+    public boolean isShow() {
+        return mCurrentKey != null;
+    }
+
+    public String getCurrentPkg() {
+        return mCurrentPkg;
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mIconSwitcher = (ImageSwitcher) findViewById(R.id.tickerIcon_ext);
+        mTextSwitcher = (TextSwitcher) findViewById(R.id.tickerText_ext);
+    }
+
+    @Override
+    public void onDarkChanged(ArrayList<Rect> areas, float darkIntensity, int tint) {
+        final int colorTint = DarkIconDispatcher.getTint(areas, this, tint);
+        if (mTextSwitcher != null) {
+            for (int i = 0; i < mTextSwitcher.getChildCount(); i++) {
+                final TextView child = (TextView) mTextSwitcher.getChildAt(i);
+                child.setTextColor(colorTint);
+            }
+        }
+        if (mIconSwitcher != null) {
+            for (int i = 0; i < mIconSwitcher.getChildCount(); i++) {
+                final ImageView child = (ImageView) mIconSwitcher.getChildAt(i);
+                child.setColorFilter(colorTint, SRC_IN);
+            }
+        }
+    }
+
+    @Override
+    public void setVisibility(int visibility) {
+        super.setVisibility(visibility);
+        mIconSwitcher.setVisibility(visibility);
+        mTextSwitcher.setVisibility(visibility);
+    }
+}

SystemUI/src/org/nameless/systemui/statusbar/ticker/MarqueeTickerEx.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.systemui.statusbar.ticker;
+
+import static org.nameless.os.DebugConstants.DEBUG_TICKER;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.plugins.DarkIconDispatcher;
+import com.android.systemui.res.R;
+import com.android.systemui.statusbar.policy.ClockCenter;
+
+public class MarqueeTickerEx extends MarqueeTicker implements Animation.AnimationListener {
+
+    private static final String TAG = "Ticker::MarqueeTickerEx";
+
+    private final Context mContext;
+
+    private AdvertSwitcherView mSwitcherView;
+    private ClockCenter mCenterClockView;
+    private View mStatusBarContents;
+    private View mTickerView;
+
+    public MarqueeTickerEx(Context context, View sb) {
+        super(context, sb);
+        mContext = context;
+    }
+
+    public void setCenterClockView(ClockCenter centerClockView) {
+        mCenterClockView = centerClockView;
+    }
+
+    public void setStatusBarContents(View statusBarContents) {
+        mStatusBarContents = statusBarContents;
+    }
+
+    public void setSwitcherView(AdvertSwitcherView switcherView) {
+        mSwitcherView = switcherView;
+    }
+
+    public void setTickerView(View tickerView) {
+        mTickerView = tickerView;
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        mTicking = false;
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+    }
+
+    @Override
+    public void tickerStarting() {
+        if (DEBUG_TICKER) {
+            Log.d(TAG, "tickerStarting");
+        }
+        mTicking = true;
+        ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).addDarkReceiver(this);
+        mStatusBarContents.setVisibility(View.GONE);
+        mTickerView.setVisibility(View.VISIBLE);
+        mStatusBarContents.startAnimation(loadAnim(R.anim.marquee_fade_out, null));
+        mTickerView.startAnimation(loadAnim(R.anim.marquee_push_down_in, null));
+        if (mCenterClockView.isCenterClock()) {
+            mCenterClockView.setVisibility(View.GONE);
+            mCenterClockView.startAnimation(loadAnim(R.anim.marquee_fade_out, null));
+        }
+        mCenterClockView.setVisibilityLocked(true);
+    }
+
+    @Override
+    public void tickerDone() {
+        if (DEBUG_TICKER) {
+            Log.d(TAG, "tickerDone");
+        }
+        mTicking = false;
+        ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).removeDarkReceiver(this);
+        mStatusBarContents.setVisibility(View.VISIBLE);
+        mTickerView.setVisibility(View.GONE);
+        mStatusBarContents.startAnimation(loadAnim(R.anim.marquee_fade_in, null));
+        mTickerView.startAnimation(loadAnim(R.anim.marquee_push_up_out, this));
+        mCenterClockView.setVisibilityLocked(false);
+        if (!mSwitcherView.isShow() && mCenterClockView.isCenterClock()) {
+            mCenterClockView.setVisibility(View.VISIBLE);
+            mCenterClockView.startAnimation(loadAnim(R.anim.marquee_fade_in, null));
+        }
+    }
+
+    @Override
+    public void tickerHalting() {
+        if (DEBUG_TICKER) {
+            Log.d(TAG, "tickerHalting");
+        }
+        mTicking = false;
+        if (mStatusBarContents.getVisibility() != View.VISIBLE) {
+            ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).removeDarkReceiver(this);
+            mStatusBarContents.setVisibility(View.VISIBLE);
+            mTickerView.setVisibility(View.GONE);
+            mStatusBarContents.startAnimation(loadAnim(android.R.anim.fade_in, null));
+            mCenterClockView.setVisibilityLocked(false);
+            if (!mSwitcherView.isShow() && mCenterClockView.isCenterClock()) {
+                mCenterClockView.setVisibility(View.VISIBLE);
+                mCenterClockView.startAnimation(loadAnim(R.anim.marquee_fade_in, null));
+            }
+        }
+    }
+
+    public Animation loadAnim(int id, Animation.AnimationListener listener) {
+        final Animation anim = AnimationUtils.loadAnimation(mContext, id);
+        if (listener != null) {
+            anim.setAnimationListener(listener);
+        }
+        return anim;
+    }
+}

SystemUI/src/org/nameless/systemui/statusbar/ticker/MarqueeTickerView.java
@@ -1,0 +1,100 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.systemui.statusbar.ticker;
+
+import static org.nameless.os.DebugConstants.DEBUG_TICKER;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.plugins.DarkIconDispatcher;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class MarqueeTickerView extends TextSwitcher implements DarkIconDispatcher.DarkReceiver {
+
+    private static final String TAG = "Ticker::MarqueeTickerView";
+
+    private static final long MIN_REPEAT_INTERVAL = 50L;
+
+    private CharSequence mLastText;
+    private long mLastTextSetTime;
+
+    protected MarqueeTicker mMarqueeTicker;
+
+    public MarqueeTickerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (DEBUG_TICKER) {
+            Log.d(TAG, "onSizeChanged, w=" + w + ", h=" + h);
+        }
+        if (mMarqueeTicker != null) {
+            mMarqueeTicker.reflowText();
+        }
+    }
+
+    public void setTicker(MarqueeTicker t) {
+        mMarqueeTicker = t;
+    }
+
+    public MarqueeTicker getTicker() {
+        return mMarqueeTicker;
+    }
+
+    @Override
+    public void onDarkChanged(ArrayList<Rect> areas, float darkIntensity, int tint) {
+        final int colorTint = DarkIconDispatcher.getTint(areas, this, tint);
+        for (int i = 0; i < getChildCount(); i++) {
+            final TextView tv = (TextView) getChildAt(i);
+            tv.setTextColor(colorTint);
+        }
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).addDarkReceiver(this);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).removeDarkReceiver(this);
+    }
+
+    @Override
+    public void setText(CharSequence text) {
+        final long curTime = System.currentTimeMillis();
+        if (Objects.equals(mLastText, text) && curTime - mLastTextSetTime <= MIN_REPEAT_INTERVAL) {
+            if (DEBUG_TICKER) {
+                Log.d(TAG, "ignore duplicate setText() invoke, text=" + text);
+            }
+            return;
+        }
+        super.setText(text);
+        mLastTextSetTime = curTime;
+        mLastText = text;
+    }
+
+    @Override
+    public void setCurrentText(CharSequence text) {
+        super.setCurrentText(text);
+        if (DEBUG_TICKER) {
+            Log.d(TAG, "setCurrentText, text=" + text);
+        }
+        mLastText = text;
+    }
+}

SystemUI/src/org/nameless/systemui/statusbar/ticker/TickerEx.kt
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.systemui.statusbar.ticker
+
+import android.app.Notification.FLAG_ONLY_ALERT_ONCE
+import android.util.Log
+import androidx.collection.LruCache
+import com.android.systemui.statusbar.notification.collection.NotificationEntry
+import org.nameless.os.DebugConstants.DEBUG_TICKER
+
+object TickerEx {
+
+    private const val TAG = "Ticker::TickerEx"
+
+    private val tickLruMap = object : LruCache<String, CharSequence>(1024) {
+        override fun sizeOf(key: String, value: CharSequence): Int {
+            return value.length
+        }
+    }
+
+    @JvmStatic
+    fun tickFilter(entry: NotificationEntry, isFirstTickThisNotification: Boolean, tickTask: Runnable) {
+        val notification = entry.sbn.notification ?: return
+        val tickerText = notification.tickerText ?: return
+        if (!isFirstTickThisNotification) {
+            val alertOnce = (notification.flags and FLAG_ONLY_ALERT_ONCE) != 0
+            if (alertOnce) {
+                return
+            }
+        }
+        val keyInLruMap = entry.sbn.key
+        if (tickerText == tickLruMap.put(keyInLruMap, tickerText)) {
+            if (DEBUG_TICKER) {
+                Log.d(TAG, "Skip ticker because of duplicate content, content=${tickerText}")
+            }
+        } else {
+            tickTask.run()
+        }
+    }
+
+    @JvmStatic
+    fun removeTickFilter(entry: NotificationEntry) {
+        tickLruMap.remove(entry.sbn.key)
+    }
+}

DF-Whyred/frameworks_base_a15

packages/SystemUI/src/org/sun/systemui/statusbar/ticker/AdvertTickerView.java
@@ -1,0 +1,157 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.sun.systemui.statusbar.ticker;
+
+import static android.graphics.PorterDuff.Mode.SRC_IN;
+
+import static org.sun.os.DebugConstants.DEBUG_TICKER;
+
+import android.app.Notification;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.service.notification.StatusBarNotification;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.plugins.DarkIconDispatcher;
+import com.android.systemui.res.R;
+
+import java.util.ArrayList;
+
+public class AdvertTickerView extends LinearLayout implements DarkIconDispatcher.DarkReceiver {
+
+    private static String TAG = "Ticker::AdvertTickerView";
+
+    private static String KEY_TICKER_ICON = "ticker_icon";
+    private static String KEY_TICKER_ICON_SWITCH = "ticker_icon_switch";
+
+    private ImageSwitcher mIconSwitcher;
+    private TextSwitcher mTextSwitcher;
+
+    private Drawable mTickerIconDrawable;
+    private int mTickIconId;
+
+    private String mCurrentKey;
+    private String mCurrentPkg;
+
+    public AdvertTickerView(Context context) {
+        super(context);
+    }
+
+    public AdvertTickerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AdvertTickerView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).addDarkReceiver(this);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).removeDarkReceiver(this);
+    }
+
+    public boolean addNotification(StatusBarNotification sbn) {
+        if (sbn == null) {
+            return false;
+        }
+        final Notification notification = sbn.getNotification();
+        final boolean show = (notification.flags & Notification.FLAG_ALWAYS_SHOW_TICKER) != 0
+                && notification.tickerText != null && !sbn.isClearable();
+        if (DEBUG_TICKER) {
+            Log.d(TAG, "id=" + sbn.getId() + ", show=" + show
+                    + ", tickerText=" + notification.tickerText
+                    + ", key=" + sbn.getKey() + ", currentKey=" + mCurrentKey);
+        }
+        if (mCurrentKey != null && !sbn.getKey().equals(mCurrentKey)) {
+            return false;
+        }
+        if (!show) {
+            removeNotification(sbn.getKey());
+            return false;
+        }
+
+        mCurrentKey = sbn.getKey();
+        mCurrentPkg = sbn.getPackageName();
+        mTickerIconDrawable = notification.getSmallIcon().loadDrawable(mContext);
+        mIconSwitcher.setVisibility(View.VISIBLE);
+        mIconSwitcher.setImageDrawable(mTickerIconDrawable);
+        mTextSwitcher.setText(notification.tickerText);
+
+        final MarqueeTextView currentTicker =
+                (MarqueeTextView) mTextSwitcher.getChildAt(mTextSwitcher.getDisplayedChild());
+        if (currentTicker != null) {
+            currentTicker.startScrollSoon();
+        }
+        return true;
+    }
+
+    public void removeNotification(String key) {
+        if (mCurrentKey != null && mCurrentKey.equals(key)) {
+            mCurrentKey = null;
+            mTickerIconDrawable = null;
+            mIconSwitcher.setImageDrawable(null);
+            mIconSwitcher.setVisibility(View.GONE);
+            mTextSwitcher.setText(null);
+        }
+    }
+
+    public boolean isShow() {
+        return mCurrentKey != null;
+    }
+
+    public String getCurrentPkg() {
+        return mCurrentPkg;
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mIconSwitcher = (ImageSwitcher) findViewById(R.id.tickerIcon_ext);
+        mTextSwitcher = (TextSwitcher) findViewById(R.id.tickerText_ext);
+    }
+
+    @Override
+    public void onDarkChanged(ArrayList<Rect> areas, float darkIntensity, int tint) {
+        final int colorTint = DarkIconDispatcher.getTint(areas, this, tint);
+        if (mTextSwitcher != null) {
+            for (int i = 0; i < mTextSwitcher.getChildCount(); i++) {
+                final TextView child = (TextView) mTextSwitcher.getChildAt(i);
+                child.setTextColor(colorTint);
+            }
+        }
+        if (mIconSwitcher != null) {
+            for (int i = 0; i < mIconSwitcher.getChildCount(); i++) {
+                final ImageView child = (ImageView) mIconSwitcher.getChildAt(i);
+                child.setColorFilter(colorTint, SRC_IN);
+            }
+        }
+    }
+
+    @Override
+    public void setVisibility(int visibility) {
+        super.setVisibility(visibility);
+        mIconSwitcher.setVisibility(visibility);
+        mTextSwitcher.setVisibility(visibility);
+    }
+}

packages/SystemUI/src/org/sun/systemui/statusbar/ticker/MarqueeTickerEx.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.sun.systemui.statusbar.ticker;
+
+import static org.sun.os.DebugConstants.DEBUG_TICKER;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.plugins.DarkIconDispatcher;
+import com.android.systemui.res.R;
+import com.android.systemui.statusbar.policy.ClockCenter;
+
+public class MarqueeTickerEx extends MarqueeTicker implements Animation.AnimationListener {
+
+    private static final String TAG = "Ticker::MarqueeTickerEx";
+
+    private final Context mContext;
+
+    private AdvertSwitcherView mSwitcherView;
+    private ClockCenter mCenterClockView;
+    private View mStatusBarContents;
+    private View mTickerView;
+
+    public MarqueeTickerEx(Context context, View sb) {
+        super(context, sb);
+        mContext = context;
+    }
+
+    public void setCenterClockView(ClockCenter centerClockView) {
+        mCenterClockView = centerClockView;
+    }
+
+    public void setStatusBarContents(View statusBarContents) {
+        mStatusBarContents = statusBarContents;
+    }
+
+    public void setSwitcherView(AdvertSwitcherView switcherView) {
+        mSwitcherView = switcherView;
+    }
+
+    public void setTickerView(View tickerView) {
+        mTickerView = tickerView;
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        mTicking = false;
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+    }
+
+    @Override
+    public void tickerStarting() {
+        if (DEBUG_TICKER) {
+            Log.d(TAG, "tickerStarting");
+        }
+        mTicking = true;
+        ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).addDarkReceiver(this);
+        mStatusBarContents.setVisibility(View.GONE);
+        mTickerView.setVisibility(View.VISIBLE);
+        mStatusBarContents.startAnimation(loadAnim(R.anim.marquee_fade_out, null));
+        mTickerView.startAnimation(loadAnim(R.anim.marquee_push_down_in, null));
+        if (mCenterClockView.isCenterClock()) {
+            mCenterClockView.setVisibility(View.GONE);
+            mCenterClockView.startAnimation(loadAnim(R.anim.marquee_fade_out, null));
+        }
+        mCenterClockView.setVisibilityLocked(true);
+    }
+
+    @Override
+    public void tickerDone() {
+        if (DEBUG_TICKER) {
+            Log.d(TAG, "tickerDone");
+        }
+        mTicking = false;
+        ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).removeDarkReceiver(this);
+        mStatusBarContents.setVisibility(View.VISIBLE);
+        mTickerView.setVisibility(View.GONE);
+        mStatusBarContents.startAnimation(loadAnim(R.anim.marquee_fade_in, null));
+        mTickerView.startAnimation(loadAnim(R.anim.marquee_push_up_out, this));
+        mCenterClockView.setVisibilityLocked(false);
+        if (!mSwitcherView.isShow() && mCenterClockView.isCenterClock()) {
+            mCenterClockView.setVisibility(View.VISIBLE);
+            mCenterClockView.startAnimation(loadAnim(R.anim.marquee_fade_in, null));
+        }
+    }
+
+    @Override
+    public void tickerHalting() {
+        if (DEBUG_TICKER) {
+            Log.d(TAG, "tickerHalting");
+        }
+        mTicking = false;
+        if (mStatusBarContents.getVisibility() != View.VISIBLE) {
+            ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).removeDarkReceiver(this);
+            mStatusBarContents.setVisibility(View.VISIBLE);
+            mTickerView.setVisibility(View.GONE);
+            mStatusBarContents.startAnimation(loadAnim(android.R.anim.fade_in, null));
+            mCenterClockView.setVisibilityLocked(false);
+            if (!mSwitcherView.isShow() && mCenterClockView.isCenterClock()) {
+                mCenterClockView.setVisibility(View.VISIBLE);
+                mCenterClockView.startAnimation(loadAnim(R.anim.marquee_fade_in, null));
+            }
+        }
+    }
+
+    public Animation loadAnim(int id, Animation.AnimationListener listener) {
+        final Animation anim = AnimationUtils.loadAnimation(mContext, id);
+        if (listener != null) {
+            anim.setAnimationListener(listener);
+        }
+        return anim;
+    }
+}

packages/SystemUI/src/org/sun/systemui/statusbar/ticker/MarqueeTickerView.java
@@ -1,0 +1,100 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.sun.systemui.statusbar.ticker;
+
+import static org.sun.os.DebugConstants.DEBUG_TICKER;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.plugins.DarkIconDispatcher;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class MarqueeTickerView extends TextSwitcher implements DarkIconDispatcher.DarkReceiver {
+
+    private static final String TAG = "Ticker::MarqueeTickerView";
+
+    private static final long MIN_REPEAT_INTERVAL = 50L;
+
+    private CharSequence mLastText;
+    private long mLastTextSetTime;
+
+    protected MarqueeTicker mMarqueeTicker;
+
+    public MarqueeTickerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (DEBUG_TICKER) {
+            Log.d(TAG, "onSizeChanged, w=" + w + ", h=" + h);
+        }
+        if (mMarqueeTicker != null) {
+            mMarqueeTicker.reflowText();
+        }
+    }
+
+    public void setTicker(MarqueeTicker t) {
+        mMarqueeTicker = t;
+    }
+
+    public MarqueeTicker getTicker() {
+        return mMarqueeTicker;
+    }
+
+    @Override
+    public void onDarkChanged(ArrayList<Rect> areas, float darkIntensity, int tint) {
+        final int colorTint = DarkIconDispatcher.getTint(areas, this, tint);
+        for (int i = 0; i < getChildCount(); i++) {
+            final TextView tv = (TextView) getChildAt(i);
+            tv.setTextColor(colorTint);
+        }
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).addDarkReceiver(this);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        ((DarkIconDispatcher) Dependency.get(DarkIconDispatcher.class)).removeDarkReceiver(this);
+    }
+
+    @Override
+    public void setText(CharSequence text) {
+        final long curTime = System.currentTimeMillis();
+        if (Objects.equals(mLastText, text) && curTime - mLastTextSetTime <= MIN_REPEAT_INTERVAL) {
+            if (DEBUG_TICKER) {
+                Log.d(TAG, "ignore duplicate setText() invoke, text=" + text);
+            }
+            return;
+        }
+        super.setText(text);
+        mLastTextSetTime = curTime;
+        mLastText = text;
+    }
+
+    @Override
+    public void setCurrentText(CharSequence text) {
+        super.setCurrentText(text);
+        if (DEBUG_TICKER) {
+            Log.d(TAG, "setCurrentText, text=" + text);
+        }
+        mLastText = text;
+    }
+}

packages/SystemUI/src/org/sun/systemui/statusbar/ticker/TickerEx.kt
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.sun.systemui.statusbar.ticker
+
+import android.app.Notification.FLAG_ONLY_ALERT_ONCE
+import android.util.Log
+import androidx.collection.LruCache
+import com.android.systemui.statusbar.notification.collection.NotificationEntry
+import org.sun.os.DebugConstants.DEBUG_TICKER
+
+object TickerEx {
+
+    private const val TAG = "Ticker::TickerEx"
+
+    private val tickLruMap = object : LruCache<String, CharSequence>(1024) {
+        override fun sizeOf(key: String, value: CharSequence): Int {
+            return value.length
+        }
+    }
+
+    @JvmStatic
+    fun tickFilter(entry: NotificationEntry, isFirstTickThisNotification: Boolean, tickTask: Runnable) {
+        val notification = entry.sbn.notification ?: return
+        val tickerText = notification.tickerText ?: return
+        if (!isFirstTickThisNotification) {
+            val alertOnce = (notification.flags and FLAG_ONLY_ALERT_ONCE) != 0
+            if (alertOnce) {
+                return
+            }
+        }
+        val keyInLruMap = entry.sbn.key
+        if (tickerText == tickLruMap.put(keyInLruMap, tickerText)) {
+            if (DEBUG_TICKER) {
+                Log.d(TAG, "Skip ticker because of duplicate content, content=${tickerText}")
+            }
+        } else {
+            tickTask.run()
+        }
+    }
+
+    @JvmStatic
+    fun removeTickFilter(entry: NotificationEntry) {
+        tickLruMap.remove(entry.sbn.key)
+    }
+}