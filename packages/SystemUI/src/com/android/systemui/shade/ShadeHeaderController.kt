cc6dccd85e9db6d84b114bd14df98cb3a3b61b8e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-05-11 21:27:50 +0200

    SystemUI: Refresh shade header theme on UI mode change
    This is required since we stopped reinflating QS panel on UI mode
    change [1], so light/dark theme switch no longer calls
    onThemeChanged().
    
    [1]: 0666e3adc1dd Revert "SystemUI: Re-evaluate system theme on UI mode change"
    
    Change-Id: If72e1fc60145dc7475b03f8bb2ccf1ecc8094b8f

--- packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
+++ packages/SystemUI/src/com/android/systemui/shade/ShadeHeaderController.kt
@@ -315,6 +315,9 @@ constructor(
                 mShadeCarrierGroup.updateTextAppearance(R.style.TextAppearance_QS_Status_Carriers)
                 updateResources()
             }
+            override fun onUiModeChanged() {
+                onThemeChanged()
+            }
         }
 
     private val nextAlarmCallback =
